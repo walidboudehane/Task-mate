8b62df248a0d1e3bceaaf124a6d23cd1a9d959b5
Walid Boudehane <walid.boudehane@umontreal.ca>
2024-01-14 15:59:36 -0500

    complete the data stracture (empty)
    create 4 html pages (todolist,about & contact us ) theses pages extends from the base.html file (using the MVT paradigm)
    create the folder statics which we will use it to add static files to our html pages .
    configure the views.py and urls.py

--- myproject/taskmate/todolist/urls.py
+++ myproject/taskmate/todolist/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from todolist import views
 
 urlpatterns = [
-    path('a', views.todolist ,name='todolist'),
+    path('', views.todolist ,name='todolist'),
+    path('contact', views.contact ,name='contact'),
+    path('about', views.about ,name='about'),
 ]
--- myproject/taskmate/todolist/views.py
+++ myproject/taskmate/todolist/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def todolist(request):
-    return HttpResponse("Hello to task page")
-
+    context = {'welcome_todolist':'welcome to taskmate !'}
+    return render(request,'todolist.html',context)
+def contact(request):
+    context = {'welcome_contact':'welcome to contact !'}
+    return render(request,'contact.html',context)
+def about(request):
+    context = {'welcome_about':'welcome to about !'}
+    return render(request,'about.html',context)
