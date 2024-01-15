ed9a9934ef31bdcddddd31ed63ce108ba8a1c27e
Walid Boudehane <walid.boudehane@umontreal.ca>
2024-01-14 22:38:06 -0500

    todolist page v1
    added an  form to input the tasks
    added the input data to our data QuerySet , and print it (POST ,GET)
    define the superUser admin

--- myproject/taskmate/todolist/models.py
+++ myproject/taskmate/todolist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    task=models.CharField(max_length=200)
+    done=models.BooleanField(default=False)
+    def __str__(self):
+        return self.task + '-'+str(self.done)
+class Person(models.Model):
+    name=models.CharField(max_length=10)
+    age=models.IntegerField()
+    
--- myproject/taskmate/todolist/views.py
+++ myproject/taskmate/todolist/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from  todolist.models import Task
+from todolist.forms import TaskForm
+from django.contrib import messages
 def todolist(request):
-    context = {'welcome_todolist':'welcome to taskmate !'}
-    return render(request,'todolist.html',context)
+    if request.method=="POST":
+        form=TaskForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        messages.success(request,'new task added')
+        return redirect('todolist')
+    else:
+        all_tasks=Task.objects.all()
+        return render(request,'todolist.html',{'all_tasks':all_tasks})
 def contact(request):
     context = {'welcome_contact':'welcome to contact !'}
     return render(request,'contact.html',context)
